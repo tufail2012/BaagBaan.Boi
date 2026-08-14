6db370f02224788eac9f36e337309a8b6796263b
tufail2012 <134710335+tufail2012@users.noreply.github.com>
2026-08-14 18:07:59 +0530

    build: update app version to 1.5.1
    Update version code and name, and refresh application launcher icons.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.baagbaan.boi"
     minSdk = 24
     targetSdk = 36
-    versionCode = 5
-    versionName = "1.5.0"
+    versionCode = 6
+    versionName = "1.5.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
